a2d3d9b332dbd7270938646215a3679a1442fc7b
Nic Boie <boie0025@gmail.com>
2015-03-17 10:00:35 -0500

    fix naming

--- app/workflow_definitions/fusion_workflow.rb
+++ app/workflow_definitions/fusion_workflow.rb
@@ -2,16 +2,16 @@ class FusionWorkflow < ActiveRecord::Base
   include Workflow
 
   delegate :events, to: :current_state
-  delegate :keys, to: :events, prefix: :event
+  delegate :keys, to: :events, prefix: :current_state_event
 
   # this will move to the next step in the current workflow
   def continue!
-    if event_keys.many?
+    if current_state_event_keys.many?
       raise Workflow::WorkflowError, 'the current state has multiple possible transitions'
-    elsif event_keys.none?
+    elsif current_state_event_keys.none?
       raise Workflow::WorkflowError, 'there are no more states left to transition to'
     else
-      next_event = event_keys.first
+      next_event = current_state_event_keys.first
       self.public_send("#{next_event}!")
     end
   end
